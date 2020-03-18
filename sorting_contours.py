b2e719fd3aa91dc5ac20f9697916a9a6ef1129ba
s.v.zhiltsov <s.v.zhiltsov@newcontact.su>
2020-03-18 11:55:40 +0400

    sorting contours

--- sorting_contours.py
+++ sorting_contours.py
@@ -2,6 +2,7 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+import index
 
 
 class OpenCVTest:
@@ -10,13 +11,24 @@ class OpenCVTest:
 
         ap = argparse.ArgumentParser()
         ap.add_argument("-i", "--image", required=True, help="path to input image")
-        args = vars(ap.parse_args())
+        ap.add_argument("-m", "--method", required=True, help="Sorting method")
+        self.args = vars(ap.parse_args())
 
         if resize:
-            image = index.CvTests(args["image"])
+            image = index.CvTests(self.args["image"])
             self.image = image.resize_with_imutils(400.0)
         else:
-            self.image = cv2.imread(args["image"])
+            self.image = cv2.imread(self.args["image"])
+
+        self.accum_edged = np.zeros(self.image.shape[:2], dtype="uint8")
+
+        for chan in cv2.split(self.image):
+            chan = cv2.medianBlur(chan, 11)
+            edged = cv2.Canny(chan, 50, 200)
+            self.accum_edged = cv2.bitwise_or(self.accum_edged, edged)
+
+        cv2.imshow('edged map', self.accum_edged)
+        cv2.waitKey(0)
 
     def sort_contours(self, contours, method="left-to-right"):
         reverse = False
@@ -25,8 +37,45 @@ class OpenCVTest:
         if method == "right-to-left" or method == "bottom-to-top":
             reverse = True
 
-        boundingBoxes = [cv2.boundingRect(contour) for contour in contours]
-        (contours, boundingBoxes) = zip(*sorted(zip(contours, boundingBoxes), key=lambda b: b[1][i], reverse=reverse))
+        if method == "top-to-bottom" or method == "left-to-right":
+            i = 1
 
-        return (contours, boundingBoxes)
+        bounding_boxes = [cv2.boundingRect(contour) for contour in contours]
+        (contours, bounding_boxes) = zip(*sorted(zip(contours, bounding_boxes), key=lambda b: b[1][i], reverse=reverse))
 
+        return (contours, bounding_boxes)
+
+    def draw_contour(self, image, i, c):
+
+        M = cv2.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+        cv2.putText(image, "#{}".format(i + 1), (cX - 20, cY), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
+
+        return image
+
+    def sorting_contours(self):
+
+        contours = cv2.findContours(self.accum_edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+        orig = self.image.copy()
+
+        for (i, c) in enumerate(contours):
+            orig = self.draw_contour(orig, i, c)
+
+        cv2.imshow("unsorted", orig)
+        cv2.waitKey(0)
+
+        (contours, bounding_boxes) = self.sort_contours(contours, method=self.args["method"])
+
+        for (i, c) in enumerate(contours):
+            self.draw_contour(self.image, i, c)
+
+        cv2.imshow("sorted", self.image)
+        cv2.waitKey(0)
+
+
+test = OpenCVTest(True)
+test.sorting_contours()
